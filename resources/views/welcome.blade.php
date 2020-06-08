2f497a03f6c46bb06f237902d17a5d770b393a14
張凱鈞 <thisisbirdhead@gmail.com>
2020-06-09 00:16:01 +0800

    test jenkins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravelQQ
                 </div>
 
                 <div class="links">
