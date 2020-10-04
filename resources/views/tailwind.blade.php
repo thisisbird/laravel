a6098bfcc9bbe41b6e8eb283015b00d1c409bbe5
張凱鈞 <thisisbirdhead@gmail.com>
2020-10-04 23:48:52 +0800

    tailwind.config.js新增比例單位

--- resources/views/tailwind.blade.php
+++ resources/views/tailwind.blade.php
@@ -1,34 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    {{-- <link rel="stylesheet" href="css/main.css"> --}}
-    <link rel="stylesheet" href="css/tailwind.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    {{-- <link rel="stylesheet" href="css/tailwind.css"> --}}
 </head>
+
 <body class=" bg-red-400 ">
     <div class="md:flex">
         <div class="md:flex-shrink-0">
-          <img class="rounded-lg md:w-56" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="Woman paying for a purchase">
+            <img class="rounded-lg md:w-56"
+                src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
+                alt="Woman paying for a purchase">
         </div>
         <div class="mt-4 md:mt-0 md:ml-6">
-          <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
-          <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
-          <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+            <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
+            <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding
+                customers for your new business</a>
+            <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five
+                ideas you can use to find your first customers.</p>
         </div>
-      </div>
-      <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+    </div>
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
         <div class="flex-shrink-0">
-          <img class="h-12 w-12" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="ChitChat Logo">
+            <img class="h-12 w-12"
+                src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
+                alt="ChitChat Logo">
         </div>
         <div class="ml-6 pt-1">
-          <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
-          <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
+            <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+            <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
         </div>
-      </div>
+    </div>
 
-      <a href="asd" class=" bg-orange-400  hover:bg-orange-700 p-3  active:bg-blue-600 focus:bg-red-600">Btn</a>
+    <a href="asd" class=" bg-orange-400  hover:bg-orange-700 p-3  active:bg-blue-600 focus:bg-red-600">Btn</a>
+    <div class="w-2/3">
+        <div class="relative bg-orange-500 pb-1/4">
+            <span class=" absolute">123</span>
+        </div>
+    </div>
 </body>
+
 </html>
